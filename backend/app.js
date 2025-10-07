7d7b688eda634e17db2da0632b2b99d02db8f597
Guillermo de la Torre Isunza <memo2507.delatorre@gmail.com>
2025-10-07 14:29:56 -0600

    Add backend folder / npm init - add config/db.js

---
+++ backend/app.js
@@ -0,0 +1,4 @@
+import connectDB from "./config/db.js";
+
+// Llamamos la función de conexión
+connectDB();
---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // lee el archivo .env
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Conectado correctamente a MongoDB");
+  } catch (error) {
+    console.error("Error de conexión a MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
