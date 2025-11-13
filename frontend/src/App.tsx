3e00994256f980b604e01bc3d5916abe31f0ef22
Carlos de Anda <carlosfelipedeanda@gmail.com>
2025-11-12 23:44:42 -0600

    agrega corección al widget de clima

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
-import MirrorView from './pages/MirrorView'
+import MirrorView from "./pages/MirrorView";
+import WeatherWidget from "./weather/weatherWidget/weatherWidget";
 
 function App() {
   return (
@@ -10,57 +10,28 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={
-          <MirrorView>
-            <div className="absolute top-6 left-6 pointer-events-auto text-white text-2xl">
-              Reloj
-            </div>
-            <button className="absolute bottom-6 left-6 pointer-events-auto text-black text-2xl">
-              holaaaaaa
-            </button>
-          </MirrorView>
-        } />
+        <Route
+          path="/"
+          element={
+            <MirrorView>
+              <div className="absolute top-6 left-6 pointer-events-auto text-white text-2xl">
+                Reloj
+              </div>
+
+              {/* Aquí estaba tu widget de clima */}
+              <div className="absolute top-6 right-6 pointer-events-auto">
+                <WeatherWidget />
+              </div>
+
+              <button className="absolute bottom-6 left-6 pointer-events-auto text-black text-2xl">
+                holaaaaaa
+              </button>
+            </MirrorView>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1 className='text-10xl font-bold'>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
